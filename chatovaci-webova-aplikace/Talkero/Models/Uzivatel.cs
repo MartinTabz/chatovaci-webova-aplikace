38644208f9aada57770952e857fb7b59dba3aa84
Martin Šíl <martin.sil1337@gmail.com>
2022-06-29 20:07:47 +0200

    Modely

---
+++ chatovaci-webova-aplikace/Talkero/Models/Uzivatel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talkero.Models
+{
+    public class Uzivatel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Mail { get; set; } = String.Empty;
+
+        [Required]
+        public string Nick { get; set; } = String.Empty;
+
+        [Required]
+        public string Heslo { get; set; } = String.Empty;
+
+        public bool Overen { get; set; }
+
+        public string Profilovka { get; set; } = String.Empty;
+
+        public string Banner { get; set; } = String.Empty;
+
+        public bool Zablokovan { get; set; }
+
+        public string DuvodZablokovani { get; set; } = String.Empty;
+
+        public bool ZadaOOdblokovani { get; set; }
+
+        public bool JeAdmin { get; set; }
+
+        public bool SouhlasiSPodminkami { get; set; }
+
+        public bool ZasilatNovinky { get; set; }
+
+        public string DatumNarozeni { get; set; } = String.Empty;
+    }
+}
---
+++ chatovaci-webova-aplikace/Talkero/Models/Zprava.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talkero.Models
+{
+    public class Zprava
+    {
+        [Key]
+        public int ZpravaId { get; set; }
+
+        public string ObsahZpravy { get; set; }
+
+        public string Obrazek { get; set; }   
+        
+        public DateTime DatumOdeslani { get; set; }
+    }
+}
