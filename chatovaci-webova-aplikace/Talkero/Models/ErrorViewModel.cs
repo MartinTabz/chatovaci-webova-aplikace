7cf0b3c2d3f509e8f8ebba0fb699aa243c73f983
Martin Šíl <martin.sil1337@gmail.com>
2022-06-29 18:46:53 +0200

    Založení projektu ve Visual Studiu

---
+++ chatovaci-webova-aplikace/Talkero/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Talkero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
